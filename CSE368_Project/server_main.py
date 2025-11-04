61cff2ee8b7811a07f506f21812734c020777845
aarav <aarav.kamat5@gmail.com>
2025-11-03 22:09:11 -0500

    basic server functionality added

--- CSE368_Project/server_main.py
+++ CSE368_Project/server_main.py
@@ -1,47 +1,35 @@
+# access the app's homepage via localhost:4090
+
 from flask import Flask
-import socketserver
+app = Flask(__name__.split('.')[0])
 
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
+# route for the homepage
+@app.route("/")
+def homepage():
+    return "homepage"
 
-    def __init__(self, request, client_address, server):
-        self.router = Router()
-        self.router.add_route("GET", "/hello", hello_path, True)
-        # TODO: Add your routes here
-
-        super().__init__(request, client_address, server)
-
-    def handle(self):
-
-        received_data = self.request.recv(16384)
-        request = Request(received_data)
-
-        # buffer till entire request is read
-        if (len(received_data) != 0 and "Content-Length" in request.headers):
-            while (len(request.body) < int(request.headers["Content-Length"])):
-                data = self.request.recv(16384)
-                request.body += data
-                received_data += data
-
-        print(self.client_address)
-        print("--- received data ---")
-        print(request.path)
-        print(received_data)
-        print("--- end of data ---\n\n")
-
-        self.router.route_request(request, self)
+# route for the quizzes page
 
 
-def main():
-    host = "0.0.0.0"
-    port = 8080
-    socketserver.ThreadingTCPServer.allow_reuse_address = True
+@app.route("/quizzes")
+def quiz_page():
+    return "quizzes"
 
-    server = socketserver.ThreadingTCPServer((host, port), MyTCPHandler)
+# route for the flashcards page
 
-    print("Listening on port " + str(port))
-    server.serve_forever()
+
+@app.route("/flashcards")
+def flashcard_page():
+    return "flashcards"
+
+# route for the intelligent assistant page
+
+
+@app.route("/intel_feedback")
+def intel_feedback_page():
+    return "intelligent feedback"
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True, port=4090)
