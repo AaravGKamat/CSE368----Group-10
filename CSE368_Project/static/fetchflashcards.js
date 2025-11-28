e2b40570633fbbdc3ac549c0f242460f07733bcf
aarav <aarav.kamat5@gmail.com>
2025-11-28 00:45:09 -0500

    uiuxflashdone

--- CSE368_Project/static/fetchflashcards.js
+++ CSE368_Project/static/fetchflashcards.js
@@ -17,7 +17,7 @@ async function fetch_flashcards() {
         for (let i = 0; i < len; i++) {
             let element = fc_length[i];
 
-            result += '<a href="/serve_flashcard/' + element["flashcard_name"] + '">' + element["flashcard_name"] + '</a><br>';
+            result += '<button class="button4"><span><a href="/serve_flashcard/' + element["flashcard_name"] + '">' + element["flashcard_name"] + '</a></span></button><br><br>';
         }
         final_list.innerHTML = result;
         return;
